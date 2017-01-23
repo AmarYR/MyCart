e0040eaa76c2e59398829184b9ccb05cf687c212
Ajith <ajith@istarindia.com>
2017-01-23 11:48:42 +0530

    Change To Media Player

--- app/src/main/java/assessment/android/istar/com/mycart/adapter/ClickListener.java
+++
@@ -1,8 +0,0 @@
-package assessment.android.istar.com.mycart.adapter;
-import android.view.View;
-
-public interface ClickListener {
-    void onClick(View view, int position);
-
-    void onLongClick(View view, int position);
-}
