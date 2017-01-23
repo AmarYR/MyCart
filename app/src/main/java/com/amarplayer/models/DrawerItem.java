d9c8fd4f0620def1262611fd1d9c8ec1d5216bbd
Ajith <ajith@istarindia.com>
2017-01-23 13:20:07 +0530

    Change To Media Player

---
+++ app/src/main/java/com/amarplayer/models/DrawerItem.java
@@ -0,0 +1,23 @@
+
+package com.amarplayer.models;
+
+
+import android.graphics.drawable.Drawable;
+
+public class DrawerItem {
+    String title;
+    Drawable icon;
+
+    public DrawerItem(String title, Drawable icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+}
